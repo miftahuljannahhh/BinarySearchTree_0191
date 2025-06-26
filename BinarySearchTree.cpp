02ad59826cb9365d3d023d6cbdf472e81bb54dd5
miftahuljannahhh <mj5416386@gmail.com>
2025-06-26 11:01:50 +0700

    membuat fungsi inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -91,4 +91,19 @@ public:
      }
  }
  
+ void inorder (Node *ptr)
+ {
+     if(isEmpty())
+     {
+         cout << "Tree is empty" << endl;
+         return;
+     }
+     if (ptr == nullptr)
+     return;
+ 
+     inorder(ptr->leftchild);
+     cout << ptr->info << " "; // parent
+     inorder(ptr->rightchild);
+ }
+ 
 };
